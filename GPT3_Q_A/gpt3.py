5fdc37211bf4808f994002b8a37c5d39a408c1d4
mohamedbougarn <mohamedbougarn@gmail.com>
2022-04-05 11:30:53 +0100

    translate with gpt3

--- GPT3_Q_A/gpt3.py
+++ GPT3_Q_A/gpt3.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 
-api="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"
+api="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"
 
 
 
@@ -33,3 +33,23 @@ class GPT3:
         content = response.choices[0].text.split('.')
         # print(content)
         return response.choices[0].text
+
+        # todo define :method that use GPT3 for for transtalte in language  to laguage     in CLASS named GPT3
+        def get_gpt3_translate(self, text):
+            openai.api_key = api
+
+            start_sequence = "\nA:"
+            restart_sequence = "\n\nQ: "
+
+            response = openai.Completion.create(
+                engine="text-davinci-001",
+                prompt="Translate this into 1. English\n"+text,
+                temperature=0.3,
+                max_tokens=170,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            content = response.choices[0].text.split('.')
+            # print(content)
+            return response.choices[0].text
--- GPT3_Q_A/testqpt3.py
+++ GPT3_Q_A/testqpt3.py
@@ -1,14 +1,16 @@
 import os
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
+
 
 openai.Answer.create(
-  search_model="ada",
-  model="curie",
-  question="which puppy is happy?",
-  documents=["Puppy A is happy.", "Puppy B is sad."],
-  examples_context="In 2017, U.S. life expectancy was 78.6 years.",
-  examples=[["What is human life expectancy in the United States?","78 years."]],
-  max_tokens=5,
-  stop=["\n", "<|endoftext|>"],
-)
+    search_model="ada",#beta type in openai
+    model="curie",
+    question="which puppy is happy?",
+    file="file-2ksWL61f0Q5c5vCYOLwUuhPk",
+    examples_context="In 2017, U.S. life expectancy was 78.6 years.",
+    examples=[["What is human life expectancy in the United States?", "78 years."]],
+    max_rerank=10,
+    max_tokens=5,
+    stop=["\n", "<|endoftext|>"]
+).answers
