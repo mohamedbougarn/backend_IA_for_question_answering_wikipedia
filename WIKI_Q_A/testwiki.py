ce032daa18b3b6ba1dfc7e0adaa0bc6adcfdbf1e
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-07 23:46:17 +0100

    add api wiki for question answer

--- WIKI_Q_A/testwiki.py
+++ WIKI_Q_A/testwiki.py
@@ -1,14 +1,38 @@
 import wikipedia
 
 def question_answer(question):
+    wikipedia.set_lang("fr")
     answer = wikipedia.summary(question)
     print(answer)
     return answer
 
+def question_answer1(question,lang):
+    #wikipedia.set_lang("fr")
+    wikipedia.set_lang(lang)
+    print(f"Question: {question}")
+    results = wikipedia.search(question)
 
+    page = wikipedia.page(results[0])
+    print(f"Top wiki result: {page}")
+
+    text = page.content
+
+    #reader.tokenize(question, text)  #
+    #print(f"Answer: {reader.get_answer()}")
+
+    print()
 
 wikipedia.set_lang("fr")
-answer = wikipedia.summary('kais said')
+#answer = wikipedia.summary(' la tunise')
+answer = wikipedia.search('la tunise')
+page = wikipedia.page(answer[0])
+print(f"Top wiki result: {page}")
+
+text = page.content
+
+# reader.tokenize(question, text)
+print(f"Answer: {reader.get_answer()}")
+
 print(answer)
 
 
