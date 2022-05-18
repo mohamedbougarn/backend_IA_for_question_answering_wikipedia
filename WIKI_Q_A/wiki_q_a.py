e9b9ab119e5a294c16f27d4448ac6dba8b8ef055
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-18 15:43:29 +0100

    qeustion_answer_paragraph

--- WIKI_Q_A/wiki_q_a.py
+++ WIKI_Q_A/wiki_q_a.py
@@ -35,3 +35,24 @@ class Wiki_Q_R:
 
         print()
         return text
+
+    def qeustion_answer_paragraph(self,question,lang):
+        # wikipedia.set_lang("fr")
+        # try:
+        wikipedia.set_lang(lang)
+        print(f"Question: {question}")
+        results = wikipedia.search(question)
+
+        summary = wikipedia.summary(results[0], auto_suggest=False,sentences=6)
+        print(f"Top wiki paragraph: {summary}")
+
+        # text = page.content
+
+        # reader.tokenize(question, text)  #
+        # print(f"Answer: {reader.get_answer()}")
+
+        # print()
+        return summary
+        # except:
+        #     # e='False'
+        #     return "False"
