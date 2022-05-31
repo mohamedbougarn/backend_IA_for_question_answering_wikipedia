9fef6db6060c8dc23db5d981d2beb925f0e0126e
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-31 09:40:09 +0100

    add google transe in gpt3

--- GPT3_Q_A/gpt3.py
+++ GPT3_Q_A/gpt3.py
@@ -1,9 +1,9 @@
 import os
 import openai
 import json
+from deep_translator import MyMemoryTranslator,GoogleTranslator,LingueeTranslator,LibreTranslator
 
-
-
+from googletrans import Translator
 
 
 
@@ -11,6 +11,7 @@ class GPT3:
 
 
     def __init__(self):
+
         pass
 
 
@@ -40,6 +41,27 @@ class GPT3:
         # print(content)
         return response.choices[0].text
 
+
+
+    # todo define 2: method that use GPT3 for questin responce in CLASS named GPT3
+    def get_gpt3aq_with_translate(self, text, lang):
+    # openai.api_key = api
+        #text1_1 = MyMemoryTranslator(source=lang, target="en").translate(text=text)
+        self.translator = Translator()
+        text1_1=self.translator.translate(text, src=lang,dest='en')
+        response = openai.Completion.create(
+            engine="text-davinci-001",
+            prompt=text1_1.text,
+            temperature=0.7,
+            max_tokens=170,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6
+        )
+        content = response.choices[0].text
+        # print(content)
+        translated = self.translator.translate(content, src='en', dest=lang)
+        return translated.text
 
 
     #todo define 3:method that use GPT3 for for transtalte in language  to laguage     in CLASS named GPT3
