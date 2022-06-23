aafe77f663df8452b8f856c4212c0ef80b7ee5bd
mohamedbougarn <mohamedbougarn@gmail.com>
2022-06-23 17:43:33 +0100

    translate googletrans in wikipedia

--- WIKI_Q_A/wiki_q_a.py
+++ WIKI_Q_A/wiki_q_a.py
@@ -1,5 +1,5 @@
 import  wikipedia
-
+from googletrans import Translator
 
 
 #todo define : loading model
@@ -33,7 +33,7 @@ class Wiki_Q_R:
         # reader.tokenize(question, text)  #
         # print(f"Answer: {reader.get_answer()}")
 
-        print()
+        print(  )
         return text
 
     def qeustion_answer_paragraph(self,question,lang):
@@ -56,3 +56,36 @@ class Wiki_Q_R:
         # except:
         #     # e='False'
         #     return "False"
+
+    # todo define :
+
+    def qeustion_answer_paragraph1(self,question,lang):
+        # wikipedia.set_lang("fr")
+        # try:
+        wikipedia.set_lang("fr")
+        print(f"Question: {question}")
+        results = wikipedia.search(question)
+
+        summary = wikipedia.summary(results[0], auto_suggest=False,sentences=6)
+        print(f"Top wiki paragraph: {summary}")
+
+        # text = page.content
+
+        # reader.tokenize(question, text)  #
+        # print(f"Answer: {reader.get_answer()}")
+
+        # print()
+        return summary
+
+    # todo define : methode for translate msg
+    def tranlatequestion(self,msg,lang):
+        self.translator = Translator()
+        msgr = self.translator.translate(msg, src=lang, dest='fr')
+        return msgr.text
+
+    # todo define : methode for retranslate msg
+    def tranlated(self,msg,lang):
+        self.translator = Translator()
+        msgr = self.translator.translate(msg, src='fr', dest=lang)
+        return msgr.text
+
